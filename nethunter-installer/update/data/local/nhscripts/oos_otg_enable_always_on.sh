616cac0a18d2039100f90bbc3469da22289fed67
ZaneZam <cyxman@yahoo.com>
2019-02-24 19:35:51 +0100

    Added script to disable OTG inactivity timeout on OOS roms

---
+++ nethunter-installer/update/data/local/nhscripts/oos_otg_enable_always_on.sh
@@ -0,0 +1,7 @@
+#!/system/bin/sh
+##
+# use this in a root shell to disable OTG inactivity timeout on OOS roms
+# more infos: https://www.xda-developers.com/enable-always-on-otg-oxygenos/
+##
+settings put global oneplus_otg_auto_disable 0
+
